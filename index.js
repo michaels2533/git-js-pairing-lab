e5544bcebdf36264f3e67c59e9abbeac2438e9ce
Kristopher Noel <noelkris500@gmail.com>
2024-10-02 14:57:27 -0400

    I did problem 4,6, and 8

--- index.js
+++ index.js
@@ -21,17 +21,36 @@ const multiplesOfThreeOrFive = () => {
     }
 
 }
-
+// Problem #4
+const untilNum = () => {
+    for(let i = 1; i <= n; i++); {
+        console.log(i);
+    }
+}
 
 // Problem #5
 const multiply = (num1, num2) => {
     return num1 * num2; 
 }
 
+// Problem #6
+const add(num1, num2) => {
+    if (num1 === num2) {
+      return 3 * (num1 + num2);
+    } else {
+      return num1 + num2;
+    }
+  }
+  
 
 //Problem #7
 const isNegative = (num) => {
     return num < 0 ? true : false; 
+}
+
+//Problem #8
+const triangleArea = (base, height) => {
+    return (base * height) / 2;
 }
 
 //Problem #9 
@@ -42,3 +61,6 @@ const betweenTwentyAndFourty = (num) => {
         return false;
     }
 }
+
+// Problem 10
+
