3b606a4eaf712cc4def5e9e4ec57e8e46450d098
michaels2533 <michael.stawowski@wsu.edu>
2024-10-02 13:50:02 -0400

    Updated index file

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-//Code your solutions in this file
+//Code your solutions in this fill
 
+//Problem #1 
 const fiveToOneHundred = () => {
     for(let i = 5; i < 101; i++) {
         console.log(`${i}`); 
     }
 }
+
+//Problem #2 
