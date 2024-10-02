7d661e8900b4556daf2fbefba638a336df60eb3a
michaels2533 <michael.stawowski@wsu.edu>
2024-10-02 14:26:14 -0400

    Completed problems 3,5,7,9'

--- index.js
+++ index.js
@@ -8,11 +8,37 @@ const fiveToOneHundred = () => {
 }
 
 //Problem #2 
-const multiplesOfThree = () => {
-    for(let i = 1; i <= 100; i++) {
-        if(i % 3 === 0) {
-console.log(i);
+
+
+
+
+//Problem #3 
+const multiplesOfThreeOrFive = () => {
+    for(let i = 3; i < 101; i++){
+        if((i % 3) === 0 || (i % 5) === 0){
+            console.log(`${i}`);
         }
-    }     
+    }
+
 }
-multiplesOfThree();
+
+
+// Problem #5
+const multiply = (num1, num2) => {
+    return num1 * num2; 
+}
+
+
+//Problem #7
+const isNegative = (num) => {
+    return num < 0 ? true : false; 
+}
+
+//Problem #9 
+const betweenTwentyAndFourty = (num) => {
+    if(num >= 20 && num <= 40){
+        return true; 
+    } else{
+        return false;
+    }
+}
