f39b35184aedf4b0437ed83dd3c3e53aa00244f6
Kristopher Noel <noelkris500@gmail.com>
2024-10-02 14:23:22 -0400

    I did problem 2

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ const fiveToOneHundred = () => {
 }
 
 //Problem #2 
+const multiplesOfThree = () => {
+    for(let i = 1; i <= 100; i++) {
+        if(i % 3 === 0) {
+console.log(i);
+        }
+    }     
+}
+multiplesOfThree();
